534292e4ea3ce853e9193c4c4a5bdd23d3348b28
Paschal Alumona <61502736+APTlog@users.noreply.github.com>
2024-05-01 21:43:42 +0000

    Completed square root of a number

--- app.rb
+++ app.rb
@@ -25,3 +25,13 @@ get("/square/results") do
   @the_result = @the_num ** 2
   erb(:square_results)
 end
+
+get("/square_root/new") do
+  erb(:square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_number = params.fetch("user_number").to_f
+  @the_sqrt_result  = @the_number ** 0.5
+  erb(:square_root_results)
+end
